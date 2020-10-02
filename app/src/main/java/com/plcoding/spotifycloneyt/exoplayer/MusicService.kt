b514bc91dd10a1c8f7dc324aca2f922ec638c221
Ahmed <qahmed89@gmail.com>
2020-10-02 15:57:58 +0200

    Setup NotificationManager

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
@@ -8,6 +8,7 @@ import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlayerNotificationListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -22,13 +23,13 @@ class MusicService : MediaBrowserServiceCompat() {
 
     @Inject
     lateinit var exoPlayer: SimpleExoPlayer
-
+      private lateinit var musicNotificationManager :MusicNotificationManager
     private val serviceJob = Job()
     private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
-
+    var isForegroundService = false
     override fun onCreate() {
         super.onCreate()
         val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
@@ -39,6 +40,12 @@ class MusicService : MediaBrowserServiceCompat() {
             isActive = true
         }
         sessionToken = mediaSession.sessionToken
+        musicNotificationManager = MusicNotificationManager(this,
+            mediaSession.sessionToken,
+            MusicPlayerNotificationListener(this)
+            ){
+            
+        }
         mediaSessionConnector = MediaSessionConnector(mediaSession)
         mediaSessionConnector.setPlayer(exoPlayer)
 
@@ -61,5 +68,6 @@ class MusicService : MediaBrowserServiceCompat() {
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
+
     }
 }
--- app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
@@ -2,4 +2,7 @@ package com.plcoding.spotifycloneyt.other
 
 object Constants {
     const val SONG_COLLECTION = "song"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID=1
+
 }
